cebd1d1122913ad0fc5aa1f573da232732b19c99
unknown <vinczei@BARIONPC18.barion.dev>
2015-11-26 09:31:51 +0100

    Fixed include file paths

--- library/helpers/loader.php
+++ library/helpers/loader.php
@@ -2,13 +2,10 @@
 
 include "iBarionModel.php";
 include "BarionHelper.php";
-$include_dirs = Array(dirname(__FILE__)."/../common", dirname(__FILE__)."/../models");
+$include_dirs = Array(realpath(dirname(__FILE__)."/../common"), realpath(dirname(__FILE__)."/../models"));
 foreach ($include_dirs as $dir) {
-  $handle = opendir($dir);
-  while (false !== ($file = readdir($handle))) {
-      if (endsWith($file, ".php")) {
-        include $dir."/".$file;
-      }
+  foreach(glob($dir.'/*.php') as $file) {
+      include $file;
   }
 }
 
