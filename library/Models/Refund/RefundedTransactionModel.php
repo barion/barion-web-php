e8fb62adf74fdfbb912619f2d43a1c4eca1710bd
Attila Botz <attila.botz@barion.com>
2024-06-10 16:52:56 +0200

    Update license header to 2024

--- library/Models/Refund/RefundedTransactionModel.php
+++ library/Models/Refund/RefundedTransactionModel.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2016 Barion Payment Inc. All Rights Reserved.
+ * Copyright 2024 Barion Payment Inc. All Rights Reserved.
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
